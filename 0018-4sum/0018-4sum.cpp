6f7f3aed58174a369fcf6d83e6728b772fab5092
gonestar321 <120496472+gonestar321@users.noreply.github.com>
2024-07-28 23:48:08 +0530

    Time: 23 ms (80.37%), Space: 16 MB (49.92%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -10,7 +10,7 @@ public:
             if(i > 0 && nums[i] == nums[i - 1]) continue;
 
             for(int j = i + 1; j < nums.size() - 2; ++j) {
-                if(j > i + 1 && nums[j] == nums[j - 1]) continue; // Corrected condition
+                if(j > i + 1 && nums[j] == nums[j - 1]) continue; 
 
                 int left = j + 1;
                 int right = nums.size() - 1;
