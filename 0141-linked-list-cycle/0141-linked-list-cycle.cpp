8cf301eb17af0519ff59790340a174b6686d3a55
gonestar321 <120496472+gonestar321@users.noreply.github.com>
2024-08-20 22:19:09 +0530

    Time: 7 ms (84.52%), Space: 10.9 MB (50.37%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,28 +9,17 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        //Floyd's Tortoise and Hare method
-        //fast pointer and a slow pointer
-        // we'll keep the loop on till the time fast pointer catches
-        // the slow pointer
-        // when it does... we call it a CYCLE IN LINKED LIST
-        
-         ListNode *tortoise = head, *hare = head ;
+        ListNode* tortoise = head;
+        ListNode* hare = head;
 
-        if (head == nullptr || head->next == nullptr) {
-        return false; // No cycle if the list is empty or has only one node
-    }
+        while( hare!=nullptr && hare->next!=nullptr){
+            tortoise = tortoise->next;
+            hare = hare->next->next;
 
-
-    while(hare != nullptr && hare ->next != nullptr){
-        tortoise = tortoise->next;
-        hare = hare->next->next;
-
-        if(tortoise == hare){
-            return true;
+            if(tortoise == hare){
+                return 1;
+            }
         }
-    }
-    return false;
-
+        return 0;
     }
 };
