a23fb203d86c48906c55927639c97dae6d63856d
gonestar321 <120496472+gonestar321@users.noreply.github.com>
2024-08-28 14:45:43 +0530

    Time: 7 ms (28.24%), Space: 12.4 MB (49.3%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -6,34 +6,55 @@ class Solution {
 public:
     // Modified method to take 'arr' as a parameter
     int findKthPositive(vector<int>& arr, int k) {
-        vector<int> newarr;
+//         vector<int> newarr;
 
-        // Find maximum and minimum elements
-        int maxele = *max_element(arr.begin(), arr.end());
+//         // Find maximum and minimum elements
+//         int maxele = *max_element(arr.begin(), arr.end());
 
-        // Collect positive integers not in arr up to maxele
-        for (int i = 1; i <= maxele; ++i) {
-            if (notfound(arr, i)) {
-                newarr.push_back(i);
+//         // Collect positive integers not in arr up to maxele
+//         for (int i = 1; i <= maxele; ++i) {
+//             if (notfound(arr, i)) {
+//                 newarr.push_back(i);
+//             }
+//         }
+
+//         // Continue collecting elements beyond maxele if necessary
+//         int current = maxele + 1;
+//         while (newarr.size() < k) {
+//             if (notfound(arr, current)) {
+//                 newarr.push_back(current);
+//             }
+//             ++current;
+//         }
+
+//         // Return the k-th missing positive number
+//         return newarr[k - 1];
+//     }
+
+// private:
+//     bool notfound(const vector<int>& arr, int target) {
+//         auto it = find(arr.begin(), arr.end(), target);
+//         return it == arr.end();
+
+//Optimal Approach
+
+        int n = arr.size();
+        int low = 0;
+        int high = n-1;
+
+
+        while( low<=high){
+
+            int mid =  (high+low)/2;
+            int miss_ele = arr[mid] - (mid+1);
+
+            if(miss_ele < k){
+                low = mid+1;
+
+            }else{
+                high = mid-1;
             }
         }
-
-        // Continue collecting elements beyond maxele if necessary
-        int current = maxele + 1;
-        while (newarr.size() < k) {
-            if (notfound(arr, current)) {
-                newarr.push_back(current);
-            }
-            ++current;
-        }
-
-        // Return the k-th missing positive number
-        return newarr[k - 1];
-    }
-
-private:
-    bool notfound(const vector<int>& arr, int target) {
-        auto it = find(arr.begin(), arr.end(), target);
-        return it == arr.end();
+        return (k + high +1);
     }
 };
