92f4ae40df73cf8dfc149624d013ff66b9decc50
gonestar321 <120496472+gonestar321@users.noreply.github.com>
2024-10-21 17:17:30 +0530

    Time: 0 ms (100%), Space: 27.7 MB (50.03%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int n= nums.size();
-        int dupe[n];
+        // int n= nums.size();
+        // int dupe[n];
 
        
-        // int pos = (i+k)%n;
+        // // int pos = (i+k)%n;
 
-        for(  int i = 0;  i<n ; i++){
-            int pos = (i+k)%n;
-            dupe[pos]=nums[i];
-        }
+        // for(  int i = 0;  i<n ; i++){
+        //     int pos = (i+k)%n;
+        //     dupe[pos]=nums[i];
+        // }
 
-        for(  int i = 0;  i<n ; i++){
+        // for(  int i = 0;  i<n ; i++){
             
-            nums[i] = dupe[i];
-            }
+        //     nums[i] = dupe[i];
+        //     }
 
+        int n = nums.size();
+        
+        k= k%n;
 
-
-        // int n = nums.size();
-        // k = k % n;
-
-        // reverse(nums.begin() , nums.end());
-        // reverse(nums.begin(), nums.begin()+k);
-        // reverse(nums.begin()+k , nums.end());
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
 }
 };
