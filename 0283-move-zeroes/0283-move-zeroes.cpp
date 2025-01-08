776caf90d4fb6cd5fcfca800e20bd8d8dd4cdf7f
gonestar321 <120496472+gonestar321@users.noreply.github.com>
2025-01-08 13:12:11 +0530

    Time: 71 ms (6.16%), Space: 23.9 MB (37.22%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,18 +1,58 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int zero_found ; //ptr1
-        int num ; //ptr 2
-        int n = nums.size();
-        int l = 0;
 
-        for( int i = 0; i< n ; i++){
-            if( nums[i] != 0){
-                swap(nums[l], nums[i]);
-                l++;
+        int findZero = 0;
+        int findNonZero = 0;
 
-            }
+        for(findZero ;findZero < nums.size() ; findZero++){
+                if(nums[findZero] == 0){
+                    for( findNonZero = findZero ; findNonZero < nums.size() ; findNonZero++){
+                        if(nums[findNonZero] != 0){
+                            swap(nums[findZero] , nums[findNonZero]);
+                            break;
+                        }
+                        
+                    }
+                }
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // int zero_found ; //ptr1
+        // int num ; //ptr 2
+        // int n = nums.size();
+        // int l = 0;
+
+        // for( int i = 0; i< n ; i++){
+        //     if( nums[i] != 0){
+        //         swap(nums[l], nums[i]);
+        //         l++;
+
+        //     }
+        // }
 
         // for( int i = l ; i<n ; i++){
         //     nums[i]= 0;
