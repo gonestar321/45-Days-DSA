ebfd1f110e857c92424fa52703c4f2a9395cee8a
gonestar321 <120496472+gonestar321@users.noreply.github.com>
2025-01-14 16:22:05 +0530

    Time: 0 ms (100%), Space: 23.7 MB (65.95%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -5,20 +5,33 @@ public:
         a = b;
         b= c;
     }
+
+
     void moveZeroes(vector<int>& nums) {
+        int left = 0;
             int n = nums.size();
 
+            //BRUTE APPROACH
+            // for( int i =0 ; i< n ; ++i){
 
-            for( int i =0 ; i< n ; ++i){
+            //     if( nums[i] == 0 ){
+            //         for( int j = i ; j<n ; ++j){
+            //             if(nums[ j] != 0){
+            //                 swap(nums[i], nums[j]);
+            //                 break;
+            //             }
+            //         }
+            //     }
+            // }
 
-                if( nums[i] == 0 ){
-                    for( int j = i ; j<n ; ++j){
-                        if(nums[ j] != 0){
-                            swap(nums[i], nums[j]);
-                            break;
-                        }
-                    }
-                }
+            //Optimal Approach
+        for (int i = 0; i < n ; ++i) {
+            if (nums[i] != 0) {
+                swap(nums[left], nums[i]);
+                left++;
             }
+        }
     }
+
+    
 };
