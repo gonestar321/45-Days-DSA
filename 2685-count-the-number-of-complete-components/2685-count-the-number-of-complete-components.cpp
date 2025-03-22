a24cd505fef156fb4d53ab37dd9f06adfe6e9cf0
gonestar321 <120496472+gonestar321@users.noreply.github.com>
2025-03-22 23:50:52 +0530

    Time: 44 ms (68.07%), Space: 142.5 MB (20.04%) - LeetHub

---
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(n);
+        vector<bool> visited(n, false);
+        
+        // Build adjacency list
+        for (auto& edge : edges) {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+        
+        int count = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                unordered_set<int> nodes;
+                queue<int> q;
+                q.push(i);
+                visited[i] = true;
+                
+                while (!q.empty()) {
+                    int node = q.front(); q.pop();
+                    nodes.insert(node);
+                    for (int neighbor : adj[node]) {
+                        if (!visited[neighbor]) {
+                            visited[neighbor] = true;
+                            q.push(neighbor);
+                        }
+                    }
+                }
+                
+                // Check if the component is complete
+                bool isComplete = true;
+                for (int node : nodes) {
+                    if (adj[node].size() != nodes.size() - 1) {
+                        isComplete = false;
+                        break;
+                    }
+                }
+                
+                if (isComplete) count++;
+            }
+        }
+        
+        return count;
+    }
+};
