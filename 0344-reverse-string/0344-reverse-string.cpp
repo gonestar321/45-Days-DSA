847d714222c368129165b3bbc89bd7fa3c4d3905
gonestar321 <120496472+gonestar321@users.noreply.github.com>
2025-04-06 23:51:38 +0530

    Time: 0 ms (100%), Space: 27.1 MB (97.64%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -7,12 +7,26 @@ public:
         s[b] = c ;
     }
 
-    void reverseString(vector<char>& s) {
-        //recursion se krte hain
-        int n = s.size();
-        for( int i = 0 ; i < s.size() ; i++){
-            if( i >= s.size()/2 ){ break ; }
-            swap( i , n-i-1 , s);
-        }
+    // void reverseString(vector<char>& s) {
+    //    //basic for loop se hua ye tohh
+    //     int n = s.size();
+    //     for( int i = 0 ; i < s.size() ; i++){
+    //         if( i >= s.size()/2 ){ break ; }
+    //         swap( i , n-i-1 , s);
+    //     }
+    // }
+
+    void letsRev(vector<char>& s ,int i, int n){
+
+        if( i >= n/2) return;
+
+        swap( i , n-i-1 , s);
+        letsRev( s , i+1 ,n);
     }
+
+     void reverseString(vector<char>& s) {
+        int n = s.size();
+        
+        letsRev( s , 0 , n );
+     }
 };
