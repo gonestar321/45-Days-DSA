4e64a9404c51d3242d5c4a46278c91c6973beea3
gonestar321 <120496472+gonestar321@users.noreply.github.com>
2025-04-07 17:04:38 +0530

    Time: 0 ms (100%), Space: 19.8 MB (56.92%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,41 +1,80 @@
 class Solution {
 public:
 
-//DFS karte hain revise.
+// //DFS Revision.
 
-    void countProvince( vector<vector<int>>& isConnected , vector<bool>&vis , int i ){
+//     void countProvince( vector<vector<int>>& isConnected , vector<bool>&vis , int i ){
 
-            vis[i] =true;
+//             vis[i] =true;
 
-            for( int x = 0 ; x<isConnected.size() ; x++){
-                if(isConnected[i][x]==1 && vis[x]==false){
-                    countProvince(isConnected , vis , x);
+//             for( int x = 0 ; x<isConnected.size() ; x++){
+//                 if(isConnected[i][x]==1 && vis[x]==false){
+//                     countProvince(isConnected , vis , x);
+//                 }
+//             }
+            
+//     }
+
+
+
+
+
+
+//     int findCircleNum(vector<vector<int>>& isConnected) {
+//         // isConnected is the adjacency list
+//         // we need to make a visited 2D array
+
+//         int n = isConnected.size();
+        
+//         int num = 0;
+//         vector<bool> vis(n,false);
+
+//         for( int i =0 ; i<n ; i++){
+//                 if(vis[i]==false){
+//                     countProvince(isConnected , vis , i );
+//                     num++;
+//                 }
+                
+//         }
+//         return num;
+//     }
+
+
+//BFS revision chalo LESSGO
+
+    void bfs( vector<vector<int>>& isConnected ,  vector<bool> & vis , int i ){
+        vis[i] = true;
+        queue<int> q; 
+        q.push(i);
+       
+
+        while(!q.empty()){
+
+        int node = q.front();
+        q.pop();
+            for( int j = 0; j<isConnected.size(); j++ ){
+                if(isConnected[node][j]==1 && vis[j] == false){
+                    q.push(j);
+                    vis[j] = true;
                 }
             }
-            
+        }
     }
 
 
-
-
-
-
     int findCircleNum(vector<vector<int>>& isConnected) {
-        // isConnected is the adjacency list
-        // we need to make a visited 2D array
 
         int n = isConnected.size();
-        
-        int num = 0;
-        vector<bool> vis(n,false);
+        // cout<<n<<endl;
+        vector<bool> vis(n , false );
+        int provinces = 0;
 
-        for( int i =0 ; i<n ; i++){
-                if(vis[i]==false){
-                    countProvince(isConnected , vis , i );
-                    num++;
-                }
-                
+        for( int i = 0 ; i < n ; i++){
+            if(vis[i] == false){
+                provinces++;
+                bfs(isConnected , vis , i);
+            }
         }
-        return num;
+        return provinces;
     }
 };
