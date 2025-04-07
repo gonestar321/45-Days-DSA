228299cec8a7ab0315b2490636164006017a5ae1
gonestar321 <120496472+gonestar321@users.noreply.github.com>
2025-04-07 21:20:30 +0530

    Time: 2 ms (18.03%), Space: 18.3 MB (46.18%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,45 @@
+class Solution {
+
+private:
+
+    void dfs( vector<vector<int>> & image , int i , int j , int color , vector<vector<bool>> & vis ){
+        //visit karde start node ko
+        vis[i][j] = true;
+        // record the color of node at hand
+        int target = image[i][j];
+        // color the node at hand
+        image[i][j] = color;
+        // prepare to check for 4 adjacent nodes of the selected ones
+        int delX[] = { 0 , -1 , 0 , 1 };
+        int delY[] = {-1 , 0 , 1 , 0 };
+
+        for(int k = 0 ; k < 4 ; k++ ){
+            int newRow = i + delX[k];
+            int newCol = j + delY[k];
+//check validity for domain of points , check for similar pixels , check for unvisited
+            if( newRow >=0 && newRow < image.size() && newCol >=0 && newCol < image[0].size() && image[newRow][newCol] == target && vis[newRow][newCol] == false ){
+                dfs( image , newRow , newCol , color , vis);
+            }
+        }
+        return;
+
+
+    }
+
+public:
+//DFS Code likhte hain
+
+
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int n = image.size();
+        int m = image[0].size();
+
+        // visited array banaya
+        vector<vector<bool>> vis(n , vector<bool>(m,false));
+
+        // directly start row and start column se shuru karte h
+
+        dfs(image , sr ,sc , color , vis);
+        return image;
+    }
+};
