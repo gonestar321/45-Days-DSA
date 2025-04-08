b01b04916375e9090a95b19b45f6971424133327
gonestar321 <120496472+gonestar321@users.noreply.github.com>
2025-04-08 17:20:53 +0530

    Time: 11 ms (23.04%), Space: 31.6 MB (16.9%) - LeetHub

---
+++ 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int ops = 0;
+        int n = nums.size();
+
+        while (true) {
+            unordered_set<int> seen;
+            bool hasDuplicate = false;
+
+            for (int num : nums) {
+                if (seen.count(num)) {
+                    hasDuplicate = true;
+                    break;
+                }
+                seen.insert(num);
+            }
+
+            if (!hasDuplicate) break;
+
+            // Perform the operation - remove first 3 elements (or all if < 3)
+            int toRemove = min(3, (int)nums.size());
+            nums.erase(nums.begin(), nums.begin() + toRemove);
+            ops++;
+        }
+
+        return ops;
+    }
+};
