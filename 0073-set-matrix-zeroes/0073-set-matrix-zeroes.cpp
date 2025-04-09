b17c322d6c6999154e5a68b76d787c8ffa2e1d1f
gonestar321 <120496472+gonestar321@users.noreply.github.com>
2025-04-09 20:03:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        queue<pair<int,int>> q;
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        for( int i = 0; i<n ; i++){
+            for( int j = 0 ; j< m ;j++){
+                if(matrix[i][j] == 0){
+                    q.push({i,j});
+                }
+            }
+        }
+        while(!q.empty()){
+            pair<int,int> node = q.front();
+            int rowNo = node.first;
+            int colNo = node.second;
+            // int delRow = -rowNo;
+            // int delCol = -colNo;
+            for(int i = 0 ; i <n ; i++){
+                matrix[i][colNo] = 0;
+            }
+            for(int i = 0 ; i <n ; i++){
+                matrix[rowNo][i] = 0;
+            }
+            q.pop();
+        }
+        
+    }
+};
