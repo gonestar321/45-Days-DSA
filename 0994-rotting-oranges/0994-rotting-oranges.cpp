229ea4a439730157b3870337034e7b1bcafb1d07
gonestar321 <120496472+gonestar321@users.noreply.github.com>
2025-04-11 22:19:20 +0530

    Time: 0 ms (100%), Space: 17.1 MB (28.86%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,56 +1,55 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        
+
         int n = grid.size();
         int m = grid[0].size();
 
-        queue<tuple<int, int, int>> q;
-        vector<vector<bool>> vis(n, vector<bool>(m, false));
-
-        // Multi-source BFS: saare rotten oranges (2) queue mein daal do with time = 0
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 2) {
-                    q.push({i, j, 0});
-                    vis[i][j] = true;
+        vector<vector<bool>> vis(n, vector<bool>(m,false));
+        queue<tuple<int,int,int>> q;
+        for( int i = 0 ; i < n ; i++){
+            for( int j = 0 ; j < m ; j++){
+                if(grid[i][j] == 2){
+                    q.push({i,j,0});
                 }
             }
         }
 
-        int delRow[] = {0, -1, 0, 1};
-        int delCol[] = {-1, 0, 1, 0};
-        int maxTime = 0;
+        int maxTime =0;
+        while(!q.empty()){
+            tuple<int,int,int> node = q.front();
 
-        // BFS traversal
-        while (!q.empty()) {
-            auto [row, col, time] = q.front();
             q.pop();
 
-            maxTime = max(maxTime, time);
+            auto[row , col , time] = node;
+            maxTime = max(maxTime , time);
 
-            for (int k = 0; k < 4; k++) {
+            int delRow[] = { -1 , 0 , 1 , 0 };
+            int delCol[] = { 0 , -1 , 0 , 1 };
+
+            for( int k = 0 ; k < 4 ; k++){
                 int newRow = row + delRow[k];
                 int newCol = col + delCol[k];
 
-                if (newRow >= 0 && newRow < n &&
-                    newCol >= 0 && newCol < m &&
-                    grid[newRow][newCol] == 1 &&
-                    !vis[newRow][newCol]) {
+                if( newRow >= 0 && newRow <n && newCol >=0 && newCol <m
+                    && grid[newRow][newCol] == 1 && vis[newRow][newCol]==false){
 
-                    q.push({newRow, newCol, time + 1});
-                    vis[newRow][newCol] = true;
-                    grid[newRow][newCol] = 2;
+                        q.push({ newRow , newCol , time+1});
+                        vis[newRow][newCol] = true;
+                        grid[newRow][newCol] = 2;
+
                 }
-            }
-        }
 
-        // Check if any fresh orange is still left
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1) return -1;
             }
-        }
+        
 
-        return maxTime;
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (grid[i][j] == 1) return -1;
+        }
+    }
+return maxTime;
     }
 };
