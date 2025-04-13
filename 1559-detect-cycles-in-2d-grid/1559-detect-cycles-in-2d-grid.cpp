49e763ef026fec76ce70094cee129ffb9ba99060
gonestar321 <120496472+gonestar321@users.noreply.github.com>
2025-04-13 21:45:36 +0530

    Time: 135 ms (40.78%), Space: 208.6 MB (18.45%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
@@ -1,72 +1,132 @@
 class Solution {
-
-private: 
-
-    bool bfs( vector<vector<char>>& grid , char alpha ,
-             queue<tuple<int,int,int,int>> &q , vector<vector<bool>>& vis ){
-
-            // queue is already taken by reference 
-            int n = grid.size();
-            int m = grid[0].size();
-
-            while( !q.empty() ){
-                tuple<int,int,int,int> node = q.front();
-                q.pop();
-
-                auto[ x , y , px , py ] = node;
-
-                int delX[] = { 0 , -1 , 0 , 1 };
-                int delY[] = { -1 , 0 , 1 , 0 };
-
-                for( int k = 0 ; k < 4 ; k++){
-                    int nextRow = x + delX[k];
-                    int nextCol = y + delY[k];
-
-
-                     if(nextRow >= 0 && nextRow < n && nextCol >=0 && nextCol < m && 
-                                (nextRow != px || nextCol != py ) &&// out of bounds check
-                                grid[nextRow][nextCol]== alpha && !vis[nextRow][nextCol] ){
-                                         q.push({nextRow , nextCol , x , y});
-                                         vis[nextRow][nextCol] = true;
-                                }
-                        
-                    else if (nextRow >= 0 && nextRow < n && nextCol >= 0 && nextCol < m &&
-                            vis[nextRow][nextCol] == true &&
-                            (nextRow != px || nextCol != py) && grid[nextRow][nextCol]== alpha ){
-                        return true;
-                    }
-
-                
-                }
-            } 
-            return false;
-    }
-
-
-public:
-    bool containsCycle(vector<vector<char>>& grid) {
-        
+//Optimal BFS
+    private:
+    bool bfs(vector<vector<char>>& grid , char target , int i , int j  , vector<vector<bool>>&vis){
+        queue<tuple<int,int,int,int>> q;
+        q.push({i, j , -1 , -1 });
         int n = grid.size();
         int m = grid[0].size();
+        int delX[] = { 0 , -1 , 0 , 1 };
+        int delY[] = { -1 , 0 , 1 , 0 };
 
-        vector<vector<bool>> vis(n, vector<bool>(m,false));
+        while(!q.empty()){
+            tuple<int,int,int,int> node = q.front();
+            q.pop();
+            auto[ x , y , px, py ] = node;
 
-        queue<tuple<int,int,int,int>>  q ;
-        //q.push({x,y, xp , yp}) ;
+                for( int k = 0 ; k<4 ; k++ ){
+                    int newRow = x + delX[k];
+                    int newCol = y + delY[k];
 
-        for( int i = 0 ; i < n ; i ++){
-            for ( int j = 0 ; j< m ; j++){
-                if( vis[i][j] == false ){
-                    int alpha = grid[i][j];
-                    vis[i][j] = true;
-                    q.push({i,j,-1,-1});
-                    if(bfs( grid , alpha  , q , vis )){
-                        return true;
-                    };
-
+                    if( newRow >=0 && newRow < n && newCol >= 0 && newCol < m && 
+                        (newRow!= px || newCol != py ) &&
+                            grid[newRow][newCol] == target && vis[newRow][newCol] == false    
+                        ){
+                            q.push({newRow , newCol , x ,  y });
+                            vis[newRow][newCol] = true;
+                        }
+                    
+                    else if ( newRow >=0 && newRow <n && newCol >=0 && newCol <m &&
+                               (newRow != px || newCol != py) &&
+                               grid[newRow][newCol]==target &&vis[newRow][newCol]==true  ){
+                                return true;
+                               }
                 }
-            }
+            
         }
         return false;
     }
+
+    public:
+    bool containsCycle(vector<vector<char>>& grid) {
+            int n = grid.size();
+            int m = grid[0].size();
+
+            vector<vector<bool>> vis(n ,  vector<bool>(m,false)); //visited array initialisation
+
+            for( int i = 0 ; i < n ; i++ ){
+                for( int  j = 0 ; j < m ; j++){
+                    char target = grid[i][j];
+                    if( !vis[i][j] ){
+                        if(bfs( grid ,target , i , j , vis)) return true;;
+                    }
+                }
+            }
+            return false;
+
+        
+
+
+}
 };
+//BRUTE-BFS 
+// private: 
+
+//     bool bfs( vector<vector<char>>& grid , char alpha ,
+//              queue<tuple<int,int,int,int>> &q , vector<vector<bool>>& vis ){
+
+//             // queue is already taken by reference 
+//             int n = grid.size();
+//             int m = grid[0].size();
+
+//             while( !q.empty() ){
+//                 tuple<int,int,int,int> node = q.front();
+//                 q.pop();
+
+//                 auto[ x , y , px , py ] = node;
+
+//                 int delX[] = { 0 , -1 , 0 , 1 };
+//                 int delY[] = { -1 , 0 , 1 , 0 };
+
+//                 for( int k = 0 ; k < 4 ; k++){
+//                     int nextRow = x + delX[k];
+//                     int nextCol = y + delY[k];
+
+
+//                      if(nextRow >= 0 && nextRow < n && nextCol >=0 && nextCol < m && 
+//                                 (nextRow != px || nextCol != py ) &&// out of bounds check
+//                                 grid[nextRow][nextCol]== alpha && !vis[nextRow][nextCol] ){
+//                                          q.push({nextRow , nextCol , x , y});
+//                                          vis[nextRow][nextCol] = true;
+//                                 }
+                        
+//                     else if (nextRow >= 0 && nextRow < n && nextCol >= 0 && nextCol < m &&
+//                             vis[nextRow][nextCol] == true &&
+//                             (nextRow != px || nextCol != py) && grid[nextRow][nextCol]== alpha ){
+//                         return true;
+//                     }
+
+                
+//                 }
+//             } 
+//             return false;
+//     }
+
+
+// public:
+//     bool containsCycle(vector<vector<char>>& grid) {
+        
+//         int n = grid.size();
+//         int m = grid[0].size();
+
+//         vector<vector<bool>> vis(n, vector<bool>(m,false));
+
+//         queue<tuple<int,int,int,int>>  q ;
+//         //q.push({x,y, xp , yp}) ;
+
+//         for( int i = 0 ; i < n ; i ++){
+//             for ( int j = 0 ; j< m ; j++){
+//                 if( vis[i][j] == false ){
+//                     int alpha = grid[i][j];
+//                     vis[i][j] = true;
+//                     q.push({i,j,-1,-1});
+//                     if(bfs( grid , alpha  , q , vis )){
+//                         return true;
+//                     };
+
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// };
