158f809c9abd599f8335b05420e6627639883188
gonestar321 <120496472+gonestar321@users.noreply.github.com>
2025-04-21 14:09:54 +0530

    Time: 0 ms (100%), Space: 17.3 MB (60.98%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,56 +1,50 @@
 class Solution {
 public:
-    bool bfs( int node , vector<vector<int>> &graph , vector<int>& color ){
-        queue<int> q; 
-        q.push( node );
+    bool bfs( int node , vector<vector<int>>&graph , vector<int>& color ){
 
-        while( !q.empty() ){
+        queue<int> q;
+        q.push(node);
+        color[node] = 0;
+
+        while(!q.empty()){
             int parent = q.front();
             q.pop();
-            // adjacent nodes ko push karo )=>
 
-            if( color[parent] == -1 ){
-                color[parent] = 0;
-            }
+            for( int i = 0 ; i< graph[parent].size() ; i++ ){
+                int child = graph[parent][i];
 
-            for(int i =0 ; i < graph[parent].size();i++){
-                if( color[graph[parent][i]] == color[parent] ){
-                    return false;
+                if( color[parent] == color[child] ){
+                        return false;
                 }
-                else if( color[graph[parent][i]] == -1 ){
-                    if(color[parent] == 0){
-                        color[graph[parent][i]] = 1 ;
-                        q.push(graph[parent][i]);
-                    }
-                    else{ color[graph[parent][i]] = 0;
-                            q.push(graph[parent][i]); }
+                else if(color[parent] == 0 && color[child] == -1){
+                    color[child] = 1;
+                    q.push(child);
                 }
-            
-        }
-    }
-    return true;
-    };
-
-
-
-    bool isBipartite(vector<vector<int>>& graph) {
-        //seedha seedha anuvaad hai ki ye graph 
-        // ek adjacency list hai aur kuchh nahin
-        int n = graph.size(); 
-
-        vector<bool> vis( n , false);
-        vector<int> color( n , -1 );
-
-        for( int i = 0 ; i < n ; i++ ){
-            // ye nodes par aagye hum 
-            if( color[i] == -1 ){
-                if(!bfs( i , graph , color  )){
-                    return false;
-                };
-
-
+                else if( color[parent]==1 && color[child] == -1){
+                    color[child] = 0;
+                    q.push(child);
+                }
             }
         }
         return true;
+        
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        // kal karliya thhaa
+        // hogaya thha...
+        // hojayegaa
+
+        int n = graph.size();
+
+        vector<int> color( n , -1);
+
+            for( int i = 0 ; i < n ; i++ ){
+                if(color[i] == -1){
+                    if(!bfs(i , graph , color)){
+                        return false;
+                    }
+                }
+            }
+            return true;
     }
 };
