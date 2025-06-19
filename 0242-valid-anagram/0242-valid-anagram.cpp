cd32e0c25ef4736ef7d8c16d73eaa968a14c0e06
gonestar321 <120496472+gonestar321@users.noreply.github.com>
2025-06-19 20:00:48 +0530

    Time: 3 ms (52.48%), Space: 10 MB (11.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,10 +1,53 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin() , s.end());
-        sort(t.begin() , t.end());
+        // sort(s.begin() , s.end());
+        // sort(t.begin() , t.end());
 
 
-        return s == t;
+        // return s == t;
+        // THIS WAS THE O(log(n)) TC solution
+
+        // let's do the hashmap- frequency method that takes O(n) TC 
+        
+        unordered_map< char , int > umap;
+
+        if(s.size() != t.size()){
+            return 0 ;
+        }
+
+        for( int i = 0 ; i < s.size() ; i++ ){
+            char x = s[i];
+
+            umap[x]++;
+        }
+
+
+        for( int i = 0 ; i < t.size() ; i++ ){
+            char x = t[i];
+
+            if(umap.count(x)){
+
+                umap[x]--;
+
+            }else{
+                return 0;
+            }
+
+        }
+        
+
+        for (auto pair : umap) {
+            if (pair.second != 0) {
+                return 0;
+            }
+        }
+    
+    return 1;
+
+
+
+
+
     }
 };
