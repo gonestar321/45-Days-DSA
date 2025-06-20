8e07757005247a873a6fa648ae9adadb5c3dba16
gonestar321 <120496472+gonestar321@users.noreply.github.com>
2025-06-20 14:10:37 +0530

    Time: 46 ms (22.43%), Space: 64.3 MB (20.58%) - LeetHub

--- LeetCode/Easy/0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ LeetCode/Easy/0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
+//USING SET GIVES O(nlog(n)) time complexity
+        // vector<int> answer ; 
+
+        // set<int> s( nums.begin() , nums.end());
+
+        // int n = nums.size();
+
+        // for( int i = 1 ; i <= n ; i++ ){
+        //     if( s.find(i) == s.end()){
+        //         answer.push_back(i);
+        //     }
+        // }
+        // return answer; 
+
 
         vector<int> answer ; 
 
-        set<int> s( nums.begin() , nums.end());
+        unordered_map<int,int> umap;
 
         int n = nums.size();
 
-        for( int i = 1 ; i <= n ; i++ ){
-            if( s.find(i) == s.end()){
+        for( int i = 0 ; i <n ; i++){
+            int element = nums[i];
+            umap[element]++;
+        }
+
+        for( int i = 1 ; i <= n ; i++){
+            if( umap.count( i ) == 0){
                 answer.push_back(i);
             }
         }
+
         return answer;
     }
 };
