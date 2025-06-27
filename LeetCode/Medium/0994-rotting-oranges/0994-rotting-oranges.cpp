631c2e46d44f4d58556f167d6cc31862692d1413
gonestar321 <120496472+gonestar321@users.noreply.github.com>
2025-06-27 23:38:05 +0530

    Time: 0 ms (100%), Space: 17.2 MB (21.22%) - LeetHub

--- LeetCode/Medium/0994-rotting-oranges/0994-rotting-oranges.cpp
+++ LeetCode/Medium/0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,53 +1,71 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        //BFS chalega bhaisahab lets see what to do 
+
+        int n = grid.size(); // row size
+        int m = grid[0].size(); // col size
+
+        vector<vector<bool>> vis( n , vector<bool>(m,false)); // visited copy of grid banadi
         
+        queue<pair<int,int>> timeNode;
 
-        int n = grid.size();
-        int m = grid[0].size();
+        for( int i = 0 ; i < n ; i++ ){
+            for( int j = 0 ; j< m ; j++ ){
 
-        vector<vector<bool>> vis(n, vector<bool>(m,false));
-        queue<tuple<int,int,int>> q;
+                if( grid[i][j] == 2 ){
+                        timeNode.push({i , j});
+                        vis[i][j] = true;
+                }
+            }
+        }
+
+        vector<int> dx = { 0 , -1 , 0 , 1 };
+        vector<int> dy = { -1 ,0  , 1 , 0 }; // direction vectors initialised
+
+        int globalTime = 0;
+        while(!timeNode.empty()){
+            
+
+            int nodeSize = timeNode.size();
+            
+
+            for( int i = 0 ; i < nodeSize ; i++ ){
+                auto [x,y] = timeNode.front();
+                timeNode.pop();
+
+                for( int i = 0 ;  i < 4 ; i++ ){ //per node ka 4-D checker
+                    int ny = y + dy[i];
+                    int nx = x + dx[i];
+
+                    if( nx >=0 && nx < n && ny >= 0 && ny < m && grid[nx][ny] == 1 && 
+                            !vis[nx][ny] ){
+                        timeNode.push({nx , ny });
+                        grid[nx][ny] = 2;
+                        vis[nx][ny] = true;
+                    }
+                }
+                
+
+            }
+            if( !timeNode.empty() ){
+                globalTime++;
+            }
+            
+
+        }
+
         for( int i = 0 ; i < n ; i++){
-            for( int j = 0 ; j < m ; j++){
-                if(grid[i][j] == 2){
-                    q.push({i,j,0});
+            for( int j = 0 ; j < m ; j++ ){
+                if( grid[i][j] == 1){
+                    return -1;
                 }
-            }
         }
-
-        int maxTime =0;
-        while(!q.empty()){
-            tuple<int,int,int> node = q.front();
-
-            q.pop();
-
-            auto[row , col , time] = node;
-            maxTime = max(maxTime , time);
-
-            int delRow[] = { -1 , 0 , 1 , 0 };
-            int delCol[] = { 0 , -1 , 0 , 1 };
-
-            for( int k = 0 ; k < 4 ; k++){
-                int newRow = row + delRow[k];
-                int newCol = col + delCol[k];
-
-                if( newRow >= 0 && newRow <n && newCol >=0 && newCol <m
-                    && grid[newRow][newCol] == 1 && vis[newRow][newCol]==false){
-
-                        q.push({ newRow , newCol , time+1});
-                        vis[newRow][newCol] = true;
-                        grid[newRow][newCol] = 2;
-
-                }
-
-            }
-    }
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (grid[i][j] == 1) return -1;
         }
-    }
-return maxTime;
+        
+        return globalTime;
+
+
+
     }
 };
