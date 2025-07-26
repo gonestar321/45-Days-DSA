acf3d8c1bcdfed95250acd0e83aca6efda99dbdc
gonestar321 <120496472+gonestar321@users.noreply.github.com>
2025-07-26 19:07:54 +0530

    Time: 12 ms (14.36%), Space: 30.3 MB (36.81%) - LeetHub

--- LeetCode/Easy/0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ LeetCode/Easy/0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,30 +1,44 @@
 class Solution {
 public:
-    void swap( int  i , int  j , vector<int>&nums ){
-        int b = nums[i];
-        nums[i] = nums[j];
-        nums[j] = b;
+    // void swap( int i , int j , vector<int>&nums){
+    //     int b = nums[j];
+    //     nums[j] = nums[i];
+    //     nums[i] = b ;
+    // }
 
-    }
-    vector<int> sortedSquares(vector<int>& nums) {
+    // vector<int> sortedSquares(vector<int>& nums) {   
+    //     int n = nums.size();
 
-        for( int i = 0 ; i < nums.size() ; i++){
+    //     int start = 0 ;
+    //     int end = n-1;
+        
+    //     for( int i = 0 ;  i < n ; i++){
+    //         nums[i] *= nums[i];
+    //     }
+
+    //     while(start < end ){
+
+    //         if( nums[start] > nums[end] ){
+    //             swap(start ,  end , nums);
+    //         }else if(nums[start] < nums[end]){
+    //             end--;
+    //         }
+    //     }
+
+    //     return nums;
+    // }
+
+    vector<int> sortedSquares(vector<int>& nums) { 
+        for( int i = 0 ; i < nums.size() ; i++ ){
+            nums[i] = abs(nums[i]);
+        }
+
+        sort(nums.begin() , nums.end());
+
+        for( int i = 0 ; i < nums.size() ; i++ ){
             nums[i] *= nums[i];
         }
+return nums;
 
-        
-        int start = 0;
-        for( int i = 0; i < nums.size() ; i++){
-            int last = nums.size() - 1 ;
-            while( start < last ){
-                
-                if( nums[start] > nums[last]){
-                    swap(start , last , nums);
-                }
-                last--;
-            }
-            
-        }
-        return nums;
     }
 };
